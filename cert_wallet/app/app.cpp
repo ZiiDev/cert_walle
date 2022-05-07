578ff1dace7c60a2c0e0bae680827c3c6cfe797c
i201965@nu.edu.pk <i201965@nu.edu.pk>
2022-05-07 05:09:57 +0500

    asda

--- cert_wallet/app/app.cpp
+++ cert_wallet/app/app.cpp
@@ -15,6 +15,7 @@
  * 
  * You should have received a copy of the GNU General Public License
  * along with SGX-WALLET.  If not, see <http://www.gnu.org/licenses/>.
+ changes
  */
 #include "enclave_u.h"
 #include "sgx_urts.h"
