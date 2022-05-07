db54f19d87e4bfaf9fc884dff5666555d9b929d9
i201965@nu.edu.pk <i201965@nu.edu.pk>
2022-05-07 05:26:05 +0500

    trial

--- cert_wallet/app/app.cpp
+++ cert_wallet/app/app.cpp
@@ -16,6 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with SGX-WALLET.  If not, see <http://www.gnu.org/licenses/>.
  changes
+ Welcome
  */
 #include "enclave_u.h"
 #include "sgx_urts.h"
